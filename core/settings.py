c232cae4eeba70db404d40daf51d0e8586aaea3c
liveshare-hub <84571962+liveshare-hub@users.noreply.github.com>
2021-06-06 16:29:24 +0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -155,8 +155,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'kuliner.mania2019@gmail.com'
-EMAIL_HOST_PASSWORD = 'Rahasia007123#$'
+EMAIL_HOST_USER = 'email_gmail'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
